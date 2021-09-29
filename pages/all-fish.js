43bea6bb44b9254833994745e6d5f5b3154e106b
Mariel de la Garza <mariel.delagarza@viget.com>
2021-09-28 20:59:26 -0400

    add list of fish names to All Fish

--- pages/all-fish.js
+++ pages/all-fish.js
@@ -1,13 +1,21 @@
 import Head from "next/head";
 import Link from "next/link";
+import { gql } from "@apollo/client";
+import client from "../apollo-client";
 
-export default function AllFish() {
+export default function AllFish({ fishes }) {
   return (
     <>
       <Head>
         <title>All Fish</title>
       </Head>
       <h1>All Fish Go Here</h1>
+      <p>All the fish</p>
+      <ul>
+        {fishes.map((fish) => (
+          <li key={fish}>{fish}</li>
+        ))}
+      </ul>
       <h2>
         <Link href="/">
           <a>Back to Home</a>
@@ -15,4 +23,25 @@ export default function AllFish() {
       </h2>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: gql`
+      query getAllFish {
+        getAllFish {
+          name
+        }
+      }
+    `,
+  });
+
+  let names = data.getAllFish.map((fish) => fish.name);
+  console.log(names);
+
+  return {
+    props: {
+      fishes: data.getAllFish.map((fish) => fish.name),
+    },
+  };
 }
