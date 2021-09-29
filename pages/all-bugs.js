03aca390b38be9f2dd54d04218b9ca959c39e1a6
Mariel de la Garza <mariel.delagarza@viget.com>
2021-09-28 20:46:33 -0400

    connect to and query backend for All Bugs
    created a new client with apollo and connected
    to backend. added simple query for all bug names
    to test connection + initial render of data, see
    how the getStaticProps function works.

--- pages/all-bugs.js
+++ pages/all-bugs.js
@@ -1,13 +1,21 @@
 import Head from "next/head";
 import Link from "next/link";
+import { gql } from "@apollo/client";
+import client from "../apollo-client";
 
-export default function AllBugs() {
+export default function AllBugs({ bugs }) {
   return (
     <>
       <Head>
         <title>All Bugs</title>
       </Head>
       <h1>All Bugs Go Here</h1>
+      <p>All the bugs</p>
+      <ul>
+        {bugs.map((bug) => (
+          <li key={bug}>{bug}</li>
+        ))}
+      </ul>
       <h2>
         <Link href="/">
           <a>Back to Home</a>
@@ -15,4 +23,25 @@ export default function AllBugs() {
       </h2>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: gql`
+      query getAllBugs {
+        getAllBugs {
+          name
+        }
+      }
+    `,
+  });
+
+  let names = data.getAllBugs.map((bug) => bug.name);
+  console.log(names);
+
+  return {
+    props: {
+      bugs: data.getAllBugs.map((bug) => bug.name),
+    },
+  };
 }
