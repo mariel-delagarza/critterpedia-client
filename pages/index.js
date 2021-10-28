1fd0118ad6636696d71e64089811cf146d9c8ddc
Mariel de la Garza <mariel.delagarza@viget.com>
2021-10-27 22:06:51 -0400

    adjust main classname

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
       </Head>
 
-      <main className="flex items-center justify-center h-screen bg-green-200">
+      <main className="flex items-center justify-center min-h-screen bg-green-200">
         <div className="container mx auto">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
             <Link href="/bugs">
