2571cba3ee3d9d779b0940bcfd419ec08400d363
Mariel de la Garza <mariel.delagarza@viget.com>
2021-11-01 20:51:15 -0400

    update table width
    Update table to not run off the page - remove x-scroll
    at the bottom, and add break-words to the months column.
    Original table setting from tailwindui page was nowrap.

--- components/table.jsx
+++ components/table.jsx
@@ -15,10 +15,10 @@ export const Table = ({ critters }) => {
 
   return (
     <div className="flex flex-col my-8">
-      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div className="-my-2 sm:-mx-6 lg:-mx-8">
         <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-            <table className="min-w-full divide-y divide-gray-200">
+          <div className="shadow overflow-visible border-b border-gray-200 sm:rounded-lg">
+            <table className="table-auto max-w-full divide-y divide-gray-200 ">
               <thead className="bg-gray-50">
                 <tr>
                   <th
@@ -71,7 +71,7 @@ export const Table = ({ critters }) => {
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                       {critter.timeRange}
                     </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                    <td className="px-6 py-4 break-words text-sm text-gray-500">
                       {critter.monthsNorth.join(", ")}
                     </td>
                   </tr>
