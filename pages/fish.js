ec06545652b038688685e9b08d1fccc412f31daf
Mariel de la Garza <mariel.delagarza@viget.com>
2021-11-08 17:09:20 -0500

    update allFish view, remove duplicate location from allBugs req

--- pages/fish.js
+++ pages/fish.js
@@ -2,27 +2,20 @@ import Head from "next/head";
 import Link from "next/link";
 import { gql } from "@apollo/client";
 import client from "../apollo-client";
+import { MonthTable } from "../components/month-table";
 
 export default function AllFish({ fishes }) {
+  console.log(fishes);
   return (
     <>
       <Head>
         <title>All Fish</title>
       </Head>
-      <h1>All Fish Go Here</h1>
-      <p>All the fish</p>
-      <ul>
-        {fishes.map(({ name, slug }) => (
-          <li key={name}>
-            <Link href={`/fish/${slug}`}>
-              <a>{name}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <h2>
+      <h1 className="my-8 text-4xl">All Fish</h1>
+      <MonthTable sortedCritters={fishes} />
+      <h2 className="my-8">
         <Link href="/">
-          <a>Back to Home</a>
+          <a className="text-4xl text-blue-700 underline">Back to Home</a>
         </Link>
       </h2>
     </>
@@ -35,7 +28,16 @@ export async function getStaticProps() {
       query getAllFish {
         getAllFish {
           name
+          shadowSize
+          fin
           slug
+          value
+          location
+          monthsNorth
+          monthsSouth
+          timeRange
+          hoursAM
+          hoursPM
         }
       }
     `,
