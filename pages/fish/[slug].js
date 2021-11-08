9a7d596921beda89d737c95132b2370d978367d5
Mariel de la Garza <mariel.delagarza@viget.com>
2021-11-08 17:03:48 -0500

    update individual view

--- pages/fish/[slug].js
+++ pages/fish/[slug].js
@@ -5,43 +5,60 @@ import { getFishData } from "../../lib/fish-data";
 export default function Fish({ fishData }) {
   return (
     <div>
-      <ul>
-        <li>
-          <h3>Name:</h3> {fishData.name}
-        </li>
-        <li>
-          <h3>Months Available (Northern Hemisphere):</h3>{" "}
-          {fishData.monthsNorth}
-        </li>
-        <li>
-          <h3>Months Available (Southern Hemisphere):</h3>{" "}
-          {fishData.monthsSouth}
-        </li>
-        <li>
-          <h3>Location:</h3> {fishData.location}
-        </li>
-        <li>
-          <h3>Value:</h3> {fishData.value}
-        </li>
-        <li>
-          <h3>Time of day available:</h3> {fishData.timeRange}
-        </li>
-        <li>
-          <h3>AM Hours Available:</h3> {fishData.hoursAM}
-        </li>
-        <li>
-          <h3>PM Hours Available:</h3> {fishData.hoursPM}
-        </li>
-        <li>
-          <h3>Has a fin?</h3> {fishData.fin}
-        </li>
-        <li>
-          <h3>Shadow size:</h3> {fishData.shadowSize}
-        </li>
-      </ul>
+      <h1 className="text-4xl leading-6 font-medium text-gray-900 my-8">
+        {fishData.name}
+      </h1>
+      <div className="bg-white shadow overflow-hidden sm:rounded-lg mx-12 mb-8">
+        <div className="border-t border-gray-200">
+          <dl>
+            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">Full name</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                {fishData.name}
+              </dd>
+            </div>
+            <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">
+                Months Available (Northern Hemisphere)
+              </dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                {fishData.monthsNorth.join(", ")}
+              </dd>
+            </div>
+            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">
+                Months Available (Southern Hemisphere)
+              </dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                {fishData.monthsSouth.join(", ")}
+              </dd>
+            </div>
+            <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">Location</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                {fishData.location}
+              </dd>
+            </div>
+            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">Value</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                {fishData.value} bells
+              </dd>
+            </div>
+            <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">
+                Time of Day Available
+              </dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                {fishData.timeRange}
+              </dd>
+            </div>
+          </dl>
+        </div>
+      </div>
       <h2>
         <Link href="/fish">
-          <a>Back to All Fish</a>
+          <a className="text-4xl text-blue-700 underline">Back to All Fish</a>
         </Link>
       </h2>
     </div>
