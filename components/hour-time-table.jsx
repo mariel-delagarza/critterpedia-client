3571f5d59328ebf2818004e66280be91e30e2b9e
Mariel de la Garza <mariel.delagarza@viget.com>
2021-11-10 21:13:46 -0500

    working form - to-do style

---
+++ components/hour-time-table.jsx
@@ -0,0 +1,56 @@
+import { CritterLink } from "./critter-link";
+
+export const HourTimeTable = ({ sortedCritters }) => {
+  return (
+    <div className="flex flex-col my-8">
+      <div className="-my-2 sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="overflow-visible sm:rounded-lg">
+            <table className="table-auto max-w-full divide-y divide-gray-200 ">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Location
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Value
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {sortedCritters.map((critter, critterIdx) => (
+                  <tr
+                    key={critter.name}
+                    className={critterIdx % 2 === 0 ? "bg-white" : "bg-gray-50"}
+                  >
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                      <CritterLink critter={critter} />
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      {critter.location}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      {critter.value}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- pages/hour-of-day.js
+++ pages/hour-of-day.js
@@ -1,29 +1,22 @@
-//import Link from "next/link";
 import { gql } from "@apollo/client";
 import client from "../apollo-client";
-//import { useRouter } from "next/router";
 import { useRouter } from "next/router";
-import HourDropdown from "../components/hour-dropdown";
-import { useEffect, useState } from "react"
-
-const initialValues = {
-  hour: "one",
-  meridian: 'AM',
-  month: "January",
-};
+import { useEffect, useState } from "react";
+import { HourTimeTable } from "../components/hour-time-table";
 
 export default function Hour({ bugs, fish }) {
   const { query, isReady } = useRouter();
-  const [params, setParams] = useState(null)
 
-  useEffect(()=>{
+  const [params, setParams] = useState(null);
+
+  useEffect(() => {
     // workaround initial empty `query` issue: https://github.com/vercel/next.js/discussions/11484
-    if(!isReady) return;
-    setParams(query)
+    if (!isReady) return;
+    setParams(query);
   }, [isReady, query]);
 
   if (!params) {
-    return null // or loading, etc. - just not an uncontrolled form input with `defaultValue`
+    return null; // or loading, etc. - just not an uncontrolled form input with `defaultValue`
   }
 
   const allCritters = bugs.concat(fish);
@@ -58,34 +51,80 @@ export default function Hour({ bugs, fish }) {
     return 0;
   });
 
-  // console.log(sortedMorningCritters);
-  // console.log(sortedAfternoonCritters);
-
-  const handleSubmit = (e) => {};
-
-
   return (
     <div>
-      <h1 className="text-4xl my-8">Form Stuff</h1>
-      <form method="GET">
-        <label>Hour: </label>
-        <select defaultValue={params.hour} name="hour">
-          <option value="one">1</option>
-          <option value="two">2</option>
-        </select>
-
-        <label>AM or PM? </label>
-        <select defaultValue={params.meridian} name="meridian">
-          <option value="AM">AM</option>
-          <option value="PM">PM</option>
-        </select>
-        <label>Month: </label>
-        <select defaultValue={params.month} name="month">
-          <option value="January">January</option>
-          <option value="February">February</option>
-        </select>
-        <button type="submit">Submit</button>
+      <h1 className="text-4xl my-8">Critters by Hour of the Day</h1>
+      <form
+        method="GET"
+        className="mt-3 grid grid-cols-1 gap-5 sm:gap-6 sm:grid-cols-2 lg:grid-cols-4"
+      >
+        <div className="flex-1 flex items-center justify-start border-solid border-4 bg-white rounded truncate">
+          <label
+            htmlFor="hour"
+            className="block text-sm font-medium text-gray-700 px-4"
+          >
+            Hour:{" "}
+          </label>
+          <select
+            id="hour"
+            name="hour"
+            className="mt-1 block pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+            defaultValue={params.hour}
+          >
+            <option value="One">1</option>
+            <option value="Two">2</option>
+            <option value="Three">3</option>
+          </select>
+        </div>
+        <div className="flex-1 flex items-center justify-start border-solid border-4 bg-white rounded truncate">
+          <label
+            htmlFor="meridian"
+            className="block text-sm font-medium text-gray-700 px-4"
+          >
+            AM or PM?
+          </label>
+          <select
+            id="meridian"
+            name="meridian"
+            className="mt-1 block pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+            defaultValue={params.meridian}
+          >
+            <option value="AM">AM</option>
+            <option value="PM">PM</option>
+          </select>
+        </div>
+        <div className="flex-1 flex items-center justify-start border-solid border-4 bg-white rounded truncate">
+          <label
+            htmlFor="month"
+            className="block text-sm font-medium text-gray-700 px-4"
+          >
+            Month:
+          </label>
+          <select
+            id="month"
+            name="month"
+            className="mt-1 block pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+            defaultValue={params.month}
+          >
+            <option value="January">January</option>
+            <option value="February">February</option>
+          </select>
+        </div>
+        <button
+          type="submit"
+          className="items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 max-w-sm"
+        >
+          Submit
+        </button>
       </form>
+      <>
+        {params.meridian == "PM" && (
+          <HourTimeTable sortedCritters={sortedAfternoonCritters} />
+        )}
+        {params.meridian == "AM" && (
+          <HourTimeTable sortedCritters={sortedMorningCritters} />
+        )}
+      </>
     </div>
   );
 }
